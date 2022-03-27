2c7c95ef03b5f2ae57e9cbdf63175c5e45a603e8
DawidRucinski <dawid_rucinski@interia.pl>
2022-03-27 21:24:18 +0200

    fixed key errors

--- keras_weights_to_caffe_model.py
+++ keras_weights_to_caffe_model.py
@@ -3,17 +3,21 @@ from tensorflow import keras
 
 
 def keras_weights_to_caffe_model(keras_model, caffe_model):
+    conv_idx=0
     for layer in keras_model.layers:
 
         # skip if there is no caffe layer named accordingly
-        if not np.any([x == layer.name for x in caffe_model._layer_names]): continue
+        if not np.any([x == layer.name for x in caffe_model._layer_names]):
+            continue
 
         if type(layer) == keras.layers.Convolution2D:
             data = layer.get_weights()
             w, b = data if np.shape(data)[0] > 1 else [data[0], np.zeros(
                 (1, np.shape(data)[-1]))]  # the bias term might not be present
-            caffe_model.params[layer.name][0].data[...] = np.transpose(w,(3, 2, 0, 1))  # Caffe wants (c_out, c_in, h, w)
-            caffe_model.params[layer.name][1].data[...] = b
+            layer_name = layer.name
+            caffe_model.params[layer_name][0].data[...] = np.transpose(w,(3, 2, 0, 1))  # Caffe wants (c_out, c_in, h, w)
+            caffe_model.params[layer_name][1].data[...] = b
+            conv_idx+=1
 
         if type(layer) == keras.layers.BatchNormalization:
             gamma, beta, mean, variance = layer.get_weights()
@@ -22,3 +26,5 @@ def keras_weights_to_caffe_model(keras_model, caffe_model):
             caffe_model.params[layer.name][2].data[...] = 1  # always set scale factor to 1
             caffe_model.params['{}_sc'.format(layer.name)][0].data[...] = gamma  # scale
             caffe_model.params['{}_sc'.format(layer.name)][1].data[...] = beta  # bias
+
+
